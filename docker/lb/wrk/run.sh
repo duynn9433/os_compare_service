fbcb16f1cf430336f2b2c568f0ec7ed0471fcc9b
Nguyen Ngoc Duy <duynn@Nguyens-MacBook-Pro.local>
2025-05-06 10:34:22 +0700

    update lb wrk script

--- docker/lb/wrk/run.sh
+++ docker/lb/wrk/run.sh
@@ -3,9 +3,10 @@
 testName=${1:""}
 threads=${2:-1}
 connections=${3:-1}
+host=${4:-"localhost:8080"}
 
 export WRK_TEST_NAME="$testName"
 
 wrk -t $threads -c $connections -d 10s --latency --timeout 5s \
     --script delay.lua \
-    http://localhost:8080
+    http://$host
